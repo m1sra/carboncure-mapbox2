b5a6583922dfb5168836cbb9448fa86323f45f8d
m1sra <105674843+m1sra@users.noreply.github.com>
2023-05-04 15:06:16 -0400

    Update Story.tsx

--- src/story-components/contentStory2-2/Story.tsx
+++ src/story-components/contentStory2-2/Story.tsx
@@ -3,7 +3,7 @@ const Story = () =>{
     <div className="story-2-2">
       <img src="icons/logo-2.png" alt="logo-story-2"/>
       <p style={{paddingTop:'10px'}}>
-      In January 2023, a historic opportunity arose to partner with Heirloom to utilize CO2 from their Direct Air Capture technology in concrete produced using CarbonCure technology, and to undertake the first ever air-to-concrete permanent CO2 storage in the United States!
+      In February 2023, a historic opportunity arose to partner with Heirloom to utilize CO2 from their Direct Air Capture (DAC) technology in concrete produced using CarbonCure technology, and to undertake the first ever air-to-concrete permanent CO2 storage in the United States!
       </p>
     </div>
   )
