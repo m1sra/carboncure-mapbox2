02a6fd38002f44d4de2502e4d535f7ac9da0e814
m1sra <105674843+m1sra@users.noreply.github.com>
2023-05-04 15:35:09 -0400

    Update Story.tsx

--- src/story-components/contentStory12/Story.tsx
+++ src/story-components/contentStory12/Story.tsx
@@ -6,7 +6,7 @@ const Story = () =>{
       As CarbonCure and Heirloom continue to scale their carbon removal technologies, processes like this will play a key role in achieving global climate goals
       </li>
       <li>
-      One day, atmospheric in concrete will be the norm, enabling widespread carbon removal wherever concrete is produced in the world and helping contribute to the decarbonization of the built environment
+      One day, atmospheric CO2 in concrete will be the norm, enabling widespread carbon removal wherever concrete is produced in the world and helping contribute to the decarbonization of the built environment.
       </li>
      </ul>
     </div>
