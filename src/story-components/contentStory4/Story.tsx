b2c9ea2ffad27ed6b37b744887be5842e62508d4
m1sra <105674843+m1sra@users.noreply.github.com>
2023-05-04 15:53:06 -0400

    Update Story.tsx

--- src/story-components/contentStory4/Story.tsx
+++ src/story-components/contentStory4/Story.tsx
@@ -1,7 +1,7 @@
 const Story = () =>{
   return (
     <div className="story-4">
-      <h2>This process is divided into two steps: regeneration and carbonation:</h2>
+      <h2>Heirloom harnesses the natural properties of limestone to remove CO2 from the atmosphere:</h2>
       <img src="icons/img-4,0.png" alt="img-story-4"/>
       <div style={{textAlign:'center'}}>
         <a href="https://www.youtube.com/watch?v=9IA950D2TsE" target="_blank">Access full video on Youtube</a>
@@ -9,14 +9,14 @@ const Story = () =>{
       
       <ul>
         <li>
-        In regeneration, calcium carbonate is placed in a renewable-powered kiln that raises the temperature to 900 degrees, decomposing the mineral into calcium oxide and enabling the capture of a high-purity stream of CO2 as a result of calcination.
+        Limestone is made up of Calcium Oxide (CaO) and CO2. When CO2 is removed from the limestone the calcium oxide wants to return to its natural limestone state. It becomes thirsty for CO2 and acts like a sponge, pulling CO2 from the atmosphere.
         </li>
         <li>
-        The calcium oxide is then hydrated and spread out on trays in a modular system to maximize its exposure to ambient air, where it undergoes carbonation: it absorbs CO2 and turns back into calcium carbonate.
+        Heirloom heats limestone mineral powder in a renewable-energy powered electric kiln to remove the CO2. The remaining calcium oxide powder is laid onto vertically-stacked trays where well trained algorithms inform Heirloom how to treat the limestone to optimize its ability to uptake CO2. The limestone mineral powder is then looped through Heirloom's process to continuously sponge CO2 from the atmosphere.
         </li>
       </ul>
       <p>
-      The process is then repeated to regularly capture atmospheric CO2 and allow it to be stored permanently.
+      Heirloom is currently capturing CO2 at its headquarters in Brisbane, CA with plans to rapidly scale their low cost Direct Air Capture technology to reach their goal of capturing and removing 1 billion tons of CO2 from the atmosphere by 2035.
       </p>
     </div>
   )
