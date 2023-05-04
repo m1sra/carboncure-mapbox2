968ecd48d840e858336a787a33f88a9d7b62b37a
m1sra <105674843+m1sra@users.noreply.github.com>
2023-05-04 15:33:06 -0400

    Update Story.tsx

--- src/story-components/contentStory11/Story.tsx
+++ src/story-components/contentStory11/Story.tsx
@@ -2,14 +2,14 @@ const Story = () =>{
   return (
     <div className="story-11">
       <p>
-      On the same day that Heirloom’s captured CO2 is injected using CarbonCure’s technology, the resulting concrete will be loaded into one of Central’s trucks and sent off to a project site
+      On the next day after CarbonCure mineralized Heirloom’s captured CO2, the resulting concrete was loaded into one of Central’s trucks and sent to a project site
       </p><img src="icons/heirloom.jpg" alt="img-story-11" style={{width:'100%'}}/>
       <ul>
         <li>
-        Once there, the concrete, now filled with CO2, will lay the foundation of yet another element of the Bay Area’s built environment, and within hours, the concrete will harden, locking away the atmospheric CO2 for thousands of years
+        Once there, the concrete, now permanently storing CO2, laid the foundation of yet another element of the Bay Area’s built environment, locking away the atmospheric CO2 for thousands of years
         </li>
         <li>
-        For the first time in history, DAC CO2 will be permanently stored in concrete
+        For the first time in history, DAC CO2 was permanently stored in concrete
         </li>
       </ul>
     </div>
